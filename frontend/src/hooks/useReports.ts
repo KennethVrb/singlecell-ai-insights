0ca502f5e512bc797a6ee8bdc0d24fa8730c4bf5
KennethVrb <kennethverbeure@gmail.com>
2025-10-05 09:34:36 +0200

    start over

--- frontend/src/hooks/useReports.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchReports } from "../api";
-
-export function useReports() {
-  return useQuery({
-    queryKey: ["reports"],
-    queryFn: fetchReports
-  });
-}
