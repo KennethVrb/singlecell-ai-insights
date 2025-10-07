c661ef53bf5db22a46e4083adccea545da04360a
KennethVrb <kennethverbeure@gmail.com>
2025-10-07 21:19:16 +0200

    django bootstrapped

---
+++ backend/singlecell_ai_insights/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Auth urls
+    path(
+        'api/auth/login/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair',
+    ),
+    path(
+        'api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'
+    ),
+    # API urls
+    path('api/', include('singlecell_ai_insights.api.urls')),
+]
